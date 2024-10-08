a82c4a58f56976ef5aa33686f239fb9d58aeb2d4
Santiago Cuervo <cuervo.santiago@gmail.com>
2024-10-08 09:02:37 -0500

    view.py: Add main frame, add main frame as entry point, add mainloop(), add PAD class attr

--- view.py
+++ view.py
@@ -4,11 +4,28 @@ Created on: 10/7/2024
 @author: sacuervo
 '''
 
+import tkinter as tk
+from tkinter import ttk
 
-class View:
+
+class View(tk.Tk):
+
+    PAD = 10
 
     def __init__(self, controller):
+        super().__init__()
         self.controller = controller
+        self.title("IberoCalc")
+        self._make_main_frame()
+        self._make_entry()
 
     def main(self):
-        print("In main of view")
+        self.mainloop()
+
+    def _make_main_frame(self):
+        self.main_frm = ttk.Frame(self)
+        self.main_frm.pack(padx=self.PAD, pady=self.PAD)
+
+    def _make_entry(self):
+        ent = ttk.Entry(self.main_frm)
+
