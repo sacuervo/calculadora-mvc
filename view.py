a51ec8ed1ebf75e32cd05dcd1d08db8a27f6ef08
Santiago Cuervo <cuervo.santiago@gmail.com>
2024-10-08 09:26:24 -0500

    view.py: Add button labels. Add button frame. Add buttons in button frame

--- view.py
+++ view.py
@@ -9,15 +9,26 @@ from tkinter import ttk
 
 
 class View(tk.Tk):
-
     PAD = 10
+
+    MAX_BUTTONS_PER_ROW = 4
+
+    button_captions = [
+        'C', '+/-', '%', '/',
+        7, 8, 9, '*',
+        4, 5, 6, '-',
+        1, 2, 3, '+',
+        0, '.', '=',
+    ]
 
     def __init__(self, controller):
         super().__init__()
-        self.controller = controller
         self.title("IberoCalc")
+        self.controller = controller
+        self.value_var = tk.StringVar()
         self._make_main_frame()
         self._make_entry()
+        self._make_buttons()
 
     def main(self):
         self.mainloop()
@@ -27,5 +38,13 @@ class View(tk.Tk):
         self.main_frm.pack(padx=self.PAD, pady=self.PAD)
 
     def _make_entry(self):
-        ent = ttk.Entry(self.main_frm)
+        ent = ttk.Entry(self.main_frm, justify='right', textvariable=self.value_var)
+        ent.pack()
 
+    def _make_buttons(self):
+        frm = ttk.Frame(self.main_frm)
+        frm.pack()
+
+        for caption in self.button_captions:
+            btn = ttk.Button(frm, text=caption)
+            btn.pack()
